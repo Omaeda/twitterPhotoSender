df5444f02dfa08bea7ec913b8f804cadb7c12f02
Omaeda <51844985+Omaeda@users.noreply.github.com>
2022-06-30 11:04:57 -0300

    working good

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import io
 import logging
 import os
 import re
@@ -5,6 +6,8 @@ import urllib.request
 from xml.dom.minidom import parseString
 
 import pyrogram
+from pyrogram.errors import MediaEmpty
+from pyrogram.types import InputMediaPhoto
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
@@ -14,19 +17,30 @@ app = pyrogram.Client("twitterUserbot", api_id=os.environ["API_ID"], api_hash=os
 @app.on_message(pyrogram.filters.outgoing & pyrogram.filters.regex(
     r"^.*twitter\.com\/(\w+)\/status\/(\d+).*$"))
 async def _(client: pyrogram.Client, message: pyrogram.types.Message):
-    img_urls = slow_mode(message)
-    if not img_urls:
-        return
+    send_as_album = ":a" in message.text
+    if send_as_album:
+        try:
+            img_urls = slow_mode(message)
+            if len(img_urls) == 1:
+                await client.send_photo(message.chat.id, img_urls[0], message.text)
+            else:
+                album = []
+                for url in img_urls:
+                    img = io.BytesIO(urllib.request.urlopen(url).read())
+                    img.name = "image.jpg"
+                    album.append(InputMediaPhoto(img, message.text if url == img_urls[0] else ""))
+                await client.send_media_group(message.chat.id, album)
+        except MediaEmpty:
+            return
+    else:
+        try:
+            await client.send_photo(message.chat.id, message.text, message.text)
+        except MediaEmpty:
+            return
+
     await message.delete()
 
-    for url in img_urls:
-        await client.send_photo(message.chat.id, url, message.text)
-
     logging.info("Processed: " + message.text)
-
-
-def fast_mode(message):
-    return [message.text]
 
 
 def slow_mode(message):
@@ -43,10 +57,10 @@ def slow_mode(message):
         if message.matches[0].group(2) in guid:
             description = item.getElementsByTagName("description")[0].firstChild.data
             img_urls = re.findall(r".*?<img src=\"([^\"]*).*?", description, re.DOTALL)
-            for url in img_urls:
-                if "video_thumb" in url or "card_img" in url:
-                    img_urls.remove(url)
             break
+    for url in img_urls:
+        if "video_thumb" in url or "card_img" in url:
+            img_urls.remove(url)
     return img_urls
 
 
