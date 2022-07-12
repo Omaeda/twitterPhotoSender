a0d079eb6aff8909e58f4a013bf9ab08046dddac
Omaeda <51844985+Omaeda@users.noreply.github.com>
2022-07-12 14:17:15 -0300

    fix for age restricted posts

--- main.py
+++ main.py
@@ -17,9 +17,23 @@ app = pyrogram.Client("twitterUserbot", api_id=os.environ["API_ID"], api_hash=os
 @app.on_message(pyrogram.filters.outgoing & pyrogram.filters.regex(
     r"^.*twitter\.com\/(\w+)\/status\/(\d+).*$"))
 async def _(client: pyrogram.Client, message: pyrogram.types.Message):
-    tweet = next(sntwitter.TwitterTweetScraper(message.matches[0].group(2)).get_items())
-    if tweet.media:
-        album = []
+    tweet_id = message.matches[0].group(2)
+    tweet = sntwitter.TwitterTweetScraper(tweet_id)
+    album = []
+    try:
+        tweet = next(tweet.get_items())
+    except KeyError as err:
+        resp = requests.get(f"https://cdn.syndication.twimg.com/tweet?id={tweet_id}").json()
+        if "photos" in resp:
+            if len(resp["photos"]) == 1:
+                album.append(resp["photos"][0]["url"])
+            else:
+                for photo in resp["photos"]:
+                    img = io.BytesIO(requests.get(photo["url"]).content)
+                    img.name = "image.jpg"
+                    album.append(InputMediaPhoto(img, message.text if media == tweet.media[0] else ""))
+
+    if getattr(tweet, "media", False):
         for media in tweet.media:
             if isinstance(media, sntwitter.Photo):
                 if len(tweet.media) == 1:
@@ -28,12 +42,12 @@ async def _(client: pyrogram.Client, message: pyrogram.types.Message):
                     img = io.BytesIO(requests.get(media.fullUrl).content)
                     img.name = "image.jpg"
                     album.append(InputMediaPhoto(img, message.text if media == tweet.media[0] else ""))
-        if not album:
-            return
-        if len(album) == 1:
-            await client.send_photo(message.chat.id, album[0], message.text)
-        else:
-            await client.send_media_group(message.chat.id, album)
+    if not album:
+        return
+    if len(album) == 1:
+        await client.send_photo(message.chat.id, album[0], message.text)
+    else:
+        await client.send_media_group(message.chat.id, album)
     await message.delete()
     logging.info("Processed: " + message.text)
 
